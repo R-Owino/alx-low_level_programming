312119e64188823edec9efa69f6199bc1e1815d0
Rehema Owino <sayeakinyi@gmail.com>
2022-09-08 13:04:05 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-/*Using puts to output a statement*/
+/**
+ * Using puts to output a statement
+ * main - main block of code
+ * Return: 0
+ */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	getchar();
 	return (0);
 }
