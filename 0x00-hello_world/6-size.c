1ae9c48d32930a4a03ee7e79a57064b133f99bf9
Rehema Owino <sayeakinyi@gmail.com>
2022-09-08 13:47:02 -0700

    Data type sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main block of code
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long c;
+	long long d;
+	float e;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+
+	return (0);
+
+}
