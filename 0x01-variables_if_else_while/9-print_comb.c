30d77b2b9d899b1a1ff486167da70543ce8c7294
Rehema Owino <sayeakinyi@gmail.com>
2022-09-09 13:08:23 -0700

    0-9 comma separated

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - output comma separated numbers 0-9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(48 + num);
+		num++;
+		if (num < 10)
+		{
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
