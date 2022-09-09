4efe0ce003ae2b8c2c25ce66e11707092ffb562a
Rehema Owino <sayeakinyi@gmail.com>
2022-09-09 11:34:36 -0700

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - output 0-9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i<10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
