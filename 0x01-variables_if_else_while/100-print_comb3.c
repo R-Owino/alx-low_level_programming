4343e57d4361b5175d0f645941b6fd77dfc238da
Rehema Owino <sayeakinyi@gmail.com>
2022-09-09 13:36:35 -0700

    Combination of 2 digits without repeat

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ * Description - output double numbers except same combinations
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j = 0;
+
+	while (j < 10)
+	{
+		i = 0;
+		while (i < 10)
+		{
+			if (j != i && j < i)
+			{
+				putchar('0' + j);
+				putchar('0' + i);
+
+				if (j + i != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			i++;
+		}
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
