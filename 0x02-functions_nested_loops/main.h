759b557e5a138a22b282553f720b6216603afc9e
Rehema Owino <sayeakinyi@gmail.com>
2022-09-13 07:01:39 -0700

    Header file for this project

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/**
+ * File: main.h
+ * Desc: Header file with all function declarations 
+ * 	 for this project's tasks.
+ */
+
+int _putchar(char p);
+
+#endif
