206c3e984580a76c782597d6254fc54776d50a8a
Rehema Owino <sayeakinyi@gmail.com>
2022-09-13 14:24:46 -0700

    Add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a, @b: carrier variables
+ * Description: Add two integers
+ * Return: int
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+	
+}
