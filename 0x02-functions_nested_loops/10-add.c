7a6312ff47375c4f1aac66d280642f30d6fa2efd
Rehema Owino <sayeakinyi@gmail.com>
2022-09-13 14:27:23 -0700

    Add 2 numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,8 @@
 
 /**
  * add - Entry point
- * @a, @b: carrier variables
+ * @a: carrier variable
+ * @b: carrier variable
  * Description: Add two integers
  * Return: int
  */
@@ -10,5 +11,5 @@
 int add(int a, int b)
 {
 	return (a + b);
-	
+
 }
