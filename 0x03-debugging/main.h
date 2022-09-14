58b861ac17e2a136e47548f32478887aef4a6d41
Rehema Owino <sayeakinyi@gmail.com>
2022-09-14 13:51:22 -0700

    Contains the prototypes for the functions in this project

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Desc: Header file with all function declarations 
+ * 	 for this project's tasks.
+ */
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+
+#endif /* MAIN_H */
