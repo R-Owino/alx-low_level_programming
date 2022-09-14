fd11b711b2b3d35305c3f84cdfb35a81a12d5eba
Rehema Owino <sayeakinyi@gmail.com>
2022-09-14 12:39:08 -0700

    Fibonacci v3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define LARGEST 10000000000
 
 /**
  * main - entry point
@@ -8,24 +9,32 @@
 
 int main(void)
 {
+	unsigned long int t1 = 0, t2 = 0, x1 = 1, x2 = 2;
+	unsigned long int temp1, temp2, temp3;
 	int i;
 
-	long int term1 = 1, term2 = 2;
-
-	long int term3 = term1 + term2;
-
-	printf("%ld, %ld, ", term1, term2);
-
-	for (i = 3; i <= 98; i++)
+	printf("%lu, %lu, ", x1, x2);
+	for (i = 2; i < 98; i++)
 	{
-		printf("%ld", term3);
-		term1 = term2;
-		term2 = term3;
-		term3 = term1 + term2;
-
-		if (i < 98)
+		if (x1 + x2 > LARGEST || t1 > 0 || t2 > 0)
+		{
+			temp1 = (x1 + x2) / LARGEST;
+			temp2 = (x1 + x2) % LARGEST;
+			temp3 = t1 + t2 + temp1;
+			t1 = t2, t2 = temp3;
+			x1 = x2, x2 = temp2;
+			printf("%lu%010lu", t2, x2);
+		}
+		else
+		{
+			temp2 = x1 + x2;
+			x1 = x2, x2 = temp2;
+			printf("%lu", x2);
+		}
+		if (i != 97)
 			printf(", ");
 	}
 	printf("\n");
 	return (0);
 }
+
