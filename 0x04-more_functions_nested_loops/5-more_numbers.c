14b1f9d9880b9d79d0ed873bbf0e6cf1c6614128
Rehema Owino <sayeakinyi@gmail.com>
2022-09-15 14:40:48 -0700

    0-14 10x

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry point
+ * Description: output 0-14 10x
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int n;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (n = 0; n < 15; n++)
+		{
+			if (n > 9)
+				_putchar(n / 10 + '0');
+
+			_putchar(n % 10 + '0');
+		}
+		_putchar('\n');
+		i++;
+	}
+}
