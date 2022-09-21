e3a5b1d12827a8aee67d8645757f98d39e69a6d7
Rehema Owino <sayeakinyi@gmail.com>
2022-09-21 15:02:56 -0700

    Capitalize a string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *string_toupper - converts a lowercase string to uppercase
+ *
+ * @str: pointer to string to capitalize
+ *
+ * Return: uppercase characters
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	for ( ; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+
+	return (str);
+
+}
