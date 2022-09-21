2545b33f72d7b5f27df0bf82623fd4d450c3721b
Rehema Owino <sayeakinyi@gmail.com>
2022-09-21 12:19:35 -0700

    Concatenate strings round 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates 2 strings using at most n bytes from src
+ *
+ * @dest: the destination string
+ *
+ * @src: string to be appended
+ *
+ * @n: number of bytes from src
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, idx = 0; /* i is the length of the destination string */
+
+	while (dest[idx++])
+		i++;
+
+	for (idx = 0; idx < n && src[idx]; idx++)
+		dest[i++] = src[idx];
+
+	return (dest);
+
+
+}
