aa087b6f5a56e1558c40af49ebc7f0f7fef6195e
Rehema Owino <sayeakinyi@gmail.com>
2022-10-01 11:09:49 -0700

    Creating and using static library

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
+/**
+ * main - print statement in puts function
+ *
+ * Return: 0 if success
+ */
+
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker
+			\"\n\t- Kevin Mitnick");
+	return (0);
 }
