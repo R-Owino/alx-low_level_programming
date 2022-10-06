6605945e7c9b44beba0384c0d31ea3d02dcdc38f
Rehema Owino <sayeakinyi@gmail.com>
2022-10-06 15:28:58 -0700

    Header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -15,5 +15,6 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+void multiply(char *s1, char *s2);
 
 #endif
