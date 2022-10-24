69a2507d4a2337787e869f431e6b0e0fe6cc45c1
Rehema Owino <sayeakinyi@gmail.com>
2022-10-24 15:24:05 -0700

    Advance 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * _hare -  prints You're beat! and yet, you must allow,\n
+ * I bore my house upon my back!\n before the main function is executed.
+ */
+
+void _hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
