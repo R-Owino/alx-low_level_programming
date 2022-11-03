40353deff41bcaf3fbed220be0d14793c8132336
Rehema Owino <sayeakinyi@gmail.com>
2022-11-03 12:19:16 -0700

    POSIX stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t op, rd, wr;
 	char *buffer;
 
-	if(filename == NULL)
+	if (filename == NULL)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
