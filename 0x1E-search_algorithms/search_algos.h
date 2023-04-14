6edee69ef6da56d97726983f0e801a0090a7a289
Rehema Owino <sayeakinyi@gmail.com>
2023-04-13 19:22:17 -0700

    Heaader file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,8 +4,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
 
+/* Mandatory tasks prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int binary_search_recursive(int *array, size_t left, size_t right, int value);
+
+/* Advanced tasks prototypes */
+int jump_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
