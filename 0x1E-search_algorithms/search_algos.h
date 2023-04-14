ad9b5e200a93cf229d31214461b728412c9f593b
Rehema Owino <sayeakinyi@gmail.com>
2023-04-13 20:06:19 -0700

    Header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,5 +13,6 @@ int binary_search_recursive(int *array, size_t left, size_t right, int value);
 
 /* Advanced tasks prototypes */
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
