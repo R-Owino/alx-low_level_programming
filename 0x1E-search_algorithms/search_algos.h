1d6d7c44674ece4e0394302f7a632d39dd952eab
R-Owino <sayeakinyi@gmail.com>
2023-04-16 19:42:12 -0700

    Header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -19,5 +19,23 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int bin_search_rec(int *array, size_t size, int value);
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
 
 #endif /* SEARCH_ALGOS_H */
