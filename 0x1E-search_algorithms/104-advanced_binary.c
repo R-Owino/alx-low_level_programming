c6cacc2213fdacfa374cdd86eb580959f64e2106
R-Owino <sayeakinyi@gmail.com>
2023-04-16 19:19:14 -0700

    Advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,46 +1,46 @@
 #include "search_algos.h"
 
 /**
- * binary_search_recursive2 - Searches for a value in an integer array using
+ * bin_search_rec - Searches for a value in an integer array using
  *	binary search algorithm recursively
  * @array: integer array to search in
- * @left: leftmost index of the subarray  to search in
- * @right: rightmost index of the subarray  to search in
+ * @size: number of elements in the array
  * @value: value to search for
  *
  * Return: index where value is located
  *	otherwise -1 if value is not present or array is NULL
  */
 
-int binary_search_recursive2(int *array, size_t left, size_t right, int value)
+int bin_search_rec(int *array, size_t size, int value)
 {
-	size_t mid = left + (right - left) / 2;
+	size_t half = size / 2;
 	size_t i;
 
-	if (left > right)
+	if (array == NULL || size == 0)
 		return (-1);
 
-	printf("Searching in array: ");
+	printf("Searching in array");
 
-	for (i = left; i <= right; i++)
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
 	{
-		printf("%d%s", array[i], (i == right) ? "\n" : ", ");
+		if (half > 0)
+			return (bin_search_rec(array, half + 1, value));
+		return ((int)half);
 	}
 
-	if (array[mid] == value)
-	{
-		/* Check if the previous element is also equal to value */
-		if (mid > left && array[mid - 1] == value)
-			/* Search in the left subarray */
-			return (binary_search_recursive2(array, left, mid - 1, value));
-		else
-			/* This is the first occurrence of value */
-			return (mid);
-	}
-	else if (array[mid] > value)
-		return (binary_search_recursive2(array, left, mid - 1, value));
-	else
-		return (binary_search_recursive2(array, mid + 1, right, value));
+	if (value < array[half])
+		return (bin_search_rec(array, half + 1, value));
+
+	half++;
+	return (bin_search_rec(array + half, size - half, value) + half);
 }
 
 /**
@@ -56,38 +56,12 @@ int binary_search_recursive2(int *array, size_t left, size_t right, int value)
 
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t left = 0;
-	size_t right = size - 1;
-	size_t i;
+	int index;
 
-	if (array == 0 || size == 0)
+	index = bin_search_rec(array, size, value);
+
+	if (index >= 0 && array[index] != value)
 		return (-1);
 
-	while (left <= right)
-	{
-		size_t mid = left + (right - left) / 2;
-
-		printf("Searching in array: ");
-
-		for (i = left; i <= right; i++)
-		{
-			printf("%d%s", array[i], (i == right) ? "\n" : ", ");
-		}
-
-		if (array[mid] == value)
-		{
-			/* Check if the previous element is also equal to value */
-			if (mid > left && array[mid - 1] == value)
-				/* Search in the left subarray */
-				right = mid - 1;
-			else
-				/* This is the first occurrence of value */
-				return (mid);
-		}
-		else if (array[mid] > value)
-			right = mid - 1;
-		else
-			left = mid + 1;
-	}
-	return (-1);
+	return (index);
 }
