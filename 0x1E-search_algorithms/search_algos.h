98fc1b99555a2afbf72820d3371d02b97545f398
R-Owino <sayeakinyi@gmail.com>
2023-04-16 19:18:51 -0700

    Header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -17,7 +17,7 @@ int interpolation_search(int *array, size_t size, int value);
 int binary_search_adv(int *array, size_t left, size_t right, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
-int binary_search_recursive2(int *array, size_t left, size_t right, int value);
+int bin_search_rec(int *array, size_t size, int value);
 
 
 #endif /* SEARCH_ALGOS_H */
